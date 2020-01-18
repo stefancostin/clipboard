46f35e34b616c2ceef46df1da7874c253d4f99ef
Stefan Costin <stefanfcostin@gmail.com>
2020-01-18 14:09:21 +0200

    Add operating system clipboard access

--- clipboard.js
+++ clipboard.js
@@ -1,8 +1,16 @@
 const ioHook = require('iohook');
+const OSClipboard = require('clipboardy');
 
 ioHook.on("keypress", event => {
   console.log(event);
   // {keychar: 'f', keycode: 19, rawcode: 15, type: 'keypress'}
+
+  OSClipboard.writeSync('🦄');
+  OSClipboard.readSync();
 });
 
 ioHook.start();
+
+// Run this as a daemon
+// Libraries: daemonize2, Forever
+
