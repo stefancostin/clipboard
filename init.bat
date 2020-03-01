54c1b7a24e7beb50cf2e75b41591b5766cb20642
stefancostin <stefanfcostin@gmail.com>
2020-03-02 01:00:10 +0200

    Remove init.bat

--- init.bat
+++
@@ -1,7 +0,0 @@
-:: %CD%         = Current Directory
-:: clipboard.js = Entry point for the Node.js application
-
-:: COMMAND FORMAT:
-:: "%CD%\path-to-local-node\node.exe clipboard.js"
-
-cmd /k "%CD%\node_runtime\node.exe clipboard.js"
